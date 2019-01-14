39be3f0e3a5ed56b9ba5f4b8333a2b28870d9b9b
Sapna3626 <46692971+Sapna3626@users.noreply.github.com>
2019-01-14 17:08:21 -0500

    Add files via upload

---
+++ Script.js
@@ -0,0 +1,14 @@
+//Name  :Sapna Devi
+//Date: January 14,2019
+//Student:C0742477
+//Course: csd 2214 section 6
+let countdown;
+let timeinterval;
+const endDate = document.querySelector('input[name="endDate"]');
+const clock = document.querySelector('clock');
+const daysSpan = clock.querySelector('.days');
+const hoursSpan = clock.querySelector('.hours');
+const minutesSpan = clock.querySelector('.minutes');
+const secondsSpan = clock.querySelector('.seconds');
+//console.log(endDate.value);
+
